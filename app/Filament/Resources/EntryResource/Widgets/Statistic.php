8983f834768baa14b114aaaf9e8d22fb6dd4cca6
ys-grouse <malsawma7777@gmail.com>
2024-02-15 08:44:41 +0530

    faet[updaet]: statistic chart -> add sum of weight and total bag, also add title

--- app/Filament/Resources/EntryResource/Widgets/Statistic.php
+++ app/Filament/Resources/EntryResource/Widgets/Statistic.php
@@ -11,35 +11,57 @@ class Statistic extends ChartWidget
 {
     protected static ?string $heading = 'Chart';
     protected static ?string $maxHeight = '50vh';
-
+    protected static ?string $pollingInterval = null;
     protected function getData(): array
     {
+
         $commodities = Commodity::query();
+        $dateRange = [
+            date('Y-m-d'),
+            date('Y-m-d 23:59:59')
+        ];
+
         $commodities = $commodities
             ->select('id', 'name')
+            ->whereHas('entries', fn ($entries) => $entries->whereBetween('created_at', $dateRange))
             ->get()->map(
                 function ($item) {
-                    $sum = $item->entries()->sum('weight_in_quintal');
-                    $item->sum_weight_today  = $sum;
-
+                    $sum_weight = $item->entries()->sum('weight_in_quintal');
+                    $vehicles = $item->entries()->count();
+                    $item->sum_of_weights  = $sum_weight;
+                    $sum_bags = $item->entries()->sum('no_of_bags');
+                    $item->sum_of_bags = $sum_bags;
+                    $label = $vehicles > 1 ? ' vehicles' : ' vehicle';
+                    $item->name = $item->name . ' (' . $vehicles  . $label . ')';
                     return $item;
                 }
             );
 
         $names = $commodities->pluck('name');
-        $data = $commodities->pluck('sum_weight_today');
-
-
+        $sum_of_weight = $commodities->pluck('sum_of_weights');
+        $sum_of_bags = $commodities->pluck('sum_of_bags');
         $date = date('d/m/Y');
+
         return [
             'datasets' => [
                 [
-                    'label' => 'Sum of each commodity weight | ' . $date,
-                    'data' => $data,
+                    'label' => 'Sum of weights',
+                    'data' => $sum_of_weight,
+                    'borderColor' => '#9BD0F5',
+                ],
+
+                [
+                    'label' => 'Total Bags',
+                    'data' => $sum_of_bags,
                 ],
             ],
-            'labels' => $names
+            'labels' => $names,
+            'title' => 'dfdf'
         ];
+    }
+    public function getDescription(): ?string
+    {
+        return  'Vairengte Supply Checkgate paltlanga Essential Commodities bungrua lo luh dan | ' . date('d/m/Y') . '.';
     }
 
     protected function getType(): string
